98c42671d43036ac36b0b5e34b70470079b38e5b
Fleur de Swift <fleurdeswift@gmail.com>
2015-08-09 12:29:15 -0700

    NSRange with CGFloat supports. NSRange.intersects
    Moved Scrollable view from CodeEditor to ExtraAppKit.

---
+++ ExtraAppKit/NSRange.swift
@@ -0,0 +1,26 @@
+//
+//  NSRange.swift
+//  ExtraAppKit
+//
+//  Copyright © 2015 Fleur de Swift. All rights reserved.
+//
+
+import Foundation
+
+public extension NSRange {
+    public init(location: CGFloat, length: CGFloat) {
+        let ix1   = Int(floor(location));
+        let ix2   = Int(ceil(location + length));
+        self.init(location: ix1, length: ix2 - ix1);
+    }
+
+    public init(start: CGFloat, end: CGFloat) {
+        let ix1   = Int(floor(start));
+        let ix2   = Int(ceil(end));
+        self.init(location: ix1, length: ix2 - ix1);
+    }
+
+    public func intersects(range2: NSRange) -> Bool {
+        return (self.location < range2.location + range2.length && range2.location < self.location + self.length);
+    }
+}
---
+++ ExtraAppKit/ScrollableView.swift
@@ -0,0 +1,108 @@
+//
+//  ScrollableView.swift
+//  CodeEditorView
+//
+//  Copyright © 2015 Fleur de Swift. All rights reserved.
+//
+
+import Cocoa
+
+/// This class make sure that it always uses all the possible space inside it's
+/// NSClipView container.
+public class ScrollableView : NSView {
+    private var lastReportedIntrinsicContentSize = NSSize(width: -1, height: -1);
+
+    public var contentSize: CGSize {
+        get {
+            return NSSize(width: 0, height: 0);
+        }
+    }
+    
+    public var clipBounds: CGSize {
+        get {
+            if let clipView = self.superview as? NSClipView {
+                return clipView.bounds.size;
+            }
+            else {
+                return self.bounds.size;
+            }
+        }
+    }
+
+    public func invalidateContentSize() {
+        if hasIntrinsicContentSizeChanged() {
+            invalidateIntrinsicContentSize();
+        }
+    }
+
+    private final func setupScrollView() {
+        if let clipView = self.superview as? NSClipView {
+            clipView.postsBoundsChangedNotifications = true;
+            clipView.postsFrameChangedNotifications = true;
+            
+            let center = NSNotificationCenter.defaultCenter();
+            
+            center.addObserver(self, selector: Selector("clipViewBoundDidChange:"), name: NSViewBoundsDidChangeNotification, object: clipView);
+            center.addObserver(self, selector: Selector("clipViewFrameDidChange:"), name: NSViewFrameDidChangeNotification,  object: clipView);
+        }
+    }
+
+    private final func hasIntrinsicContentSizeChanged() -> Bool {
+        let calculatedSize = calculateIntrinsicContentSize();
+    
+        if abs(lastReportedIntrinsicContentSize.width  - calculatedSize.width)  > 0.9 ||
+           abs(lastReportedIntrinsicContentSize.height - calculatedSize.height) > 0.9 {
+           return true;
+        }
+        
+        return false;
+    }
+
+    @objc
+    private final func clipViewFrameDidChange(notification: NSNotification) -> Void {
+        frameDidChange();
+        invalidateContentSize();
+    }
+
+    @objc
+    private final func clipViewBoundDidChange(notification: NSNotification) -> Void {
+        boundsDidChange();
+    }
+    
+    public func frameDidChange() {
+    }
+    
+    public func boundsDidChange() {
+    }
+    
+    private final func calculateIntrinsicContentSize() -> NSSize {
+        if let clipView = self.superview as? NSClipView {
+            let contentSize = self.contentSize;
+            let clipBounds  = clipView.bounds;
+            
+            return  NSSize(width:  max(contentSize.width,  clipBounds.size.width),
+                           height: max(contentSize.height, clipBounds.size.height));
+        }
+        else {
+            return self.contentSize;
+        }
+    }
+    
+    @objc
+    public final override var intrinsicContentSize: NSSize {
+        get {
+            lastReportedIntrinsicContentSize = calculateIntrinsicContentSize();
+            return lastReportedIntrinsicContentSize;
+        }
+    }
+
+    public override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect);
+        setupScrollView();
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder);
+        setupScrollView();
+    }
+}
